13648de7b3a002a355ba28f86577565806d3f4f5
Rancho <yyrancho@gmail.com>
2017-06-09 19:16:34 +0800

    Adjust the animation of the **navigation bar**
    Before: always display when  the position is below the header.
    Now: Only display when scroll up and the position is below the header.

--- blog/static/js/navigation.js
+++ blog/static/js/navigation.js
@@ -1,12 +1,15 @@
 // switch the visibility of the navigation bar
+var before_scrolltop = $(window).scrollTop()
 $(window).scroll(function() {
     var nav = $('nav')
     var nav_a = $('nav>a')
-    if ($(window).scrollTop() > 370) {
+    var after_scrolltop = $(window).scrollTop()
+    if (after_scrolltop < before_scrolltop && after_scrolltop > 390) {
         nav.css('position','fixed').css('background-color','rgba(0,133,161,0.8)')
         nav_a.css('padding-top','1.5%').css('padding-bottom','1.5%')
     } else {
         nav.css('position','').css('background-color','rgba(0,0,0,0)')
         nav_a.css('padding-top','2%').css('padding-bottom','1%')
     }
+    before_scrolltop = after_scrolltop
 })
