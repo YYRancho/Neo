14f4481224cb00ea58c32d14f1280bf3a52b38ca
Rancho <yyrancho@gmail.com>
2017-09-24 11:59:37 +0800

    feat(chatroom): add notifications of new messages
    Import Push.js. When the current tab is not active, send notifications.

--- chatroom/static/js/src/chatroom_message.js
+++ chatroom/static/js/src/chatroom_message.js
@@ -1,6 +1,5 @@
 'use strict';
 
-// initial time
 $(document).ready(function() {
     let time_now = format_time_now()
     $('.message-time').text(time_now.split('.')[0])
@@ -9,6 +8,7 @@ $(document).ready(function() {
     get_messages()
 })
 
+// initial time
 function format_time_now() {
     let date = new Date()
     let time_now = '%Y-%m-%d %H:%M:%S'
@@ -23,6 +23,15 @@ function format_time_now() {
     return time_now
 }
 
+// determine whether the current tab is active
+let active_flag = true
+window.onfocus = function() {
+    active_flag = true
+}
+window.onblur = function() {
+    active_flag = false
+}
+
 // ========== get messages ==========
 function get_messages() {
     $.ajax({
@@ -35,16 +44,6 @@ function get_messages() {
     .done(function(messages) {
         console.log('get_messages SUCCESS');
         messages.forEach(function(message) {
-            let time_str = ''
-            // determine whether the new message is sent by the user self
-            if ($('#name-editor').text() === message.sender && $('#my-ip').text() === message.sender_ip) {
-                time_str += `<div id="${message.time}" class="message-wrapper"><div class="message-time">${message.time.split('.')[0].split(' ')[1]}`
-                time_str += '&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'
-                time_str += `<span class="message-arrow">➢➣➤</span></div>`
-            } else {
-                time_str += `<div id="${message.time}" class="message-wrapper"><div class="message-time">${message.time.split('.')[0]}</div>`
-            }
-
             let content_str = `<div class="message-content"><span class="user-name">${message.sender}</span>`
             content_str += `@<span class="user-ip">${message.sender_ip}</span>`
             // determine whether the new message is file uploading
@@ -61,6 +60,28 @@ function get_messages() {
             content_str = content_str.replace(url_reg, '<a href="$1$2">$1$2</a>')
             // make all links open in a new tab
             content_str = content_str.replace(/<a(.*?)>/g, '<a target="_blank" $1>')
+
+            let time_str = ''
+            // determine whether the new message is sent by the user self
+            if ($('#name-editor').text() === message.sender && $('#my-ip').text() === message.sender_ip) {
+                time_str += `<div id="${message.time}" class="message-wrapper"><div class="message-time">${message.time.split('.')[0].split(' ')[1]}`
+                time_str += '&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'
+                time_str += `<span class="message-arrow">➢➣➤</span></div>`
+            } else {
+                time_str += `<div id="${message.time}" class="message-wrapper"><div class="message-time">${message.time.split('.')[0]}</div>`
+                // notify if the current tab is not active
+                if (!active_flag) {
+                    Push.create(`${message.sender}@${message.sender_ip}`, {
+                        body: message.text,
+                        icon: '/static/images/download.png',
+                        timeout: 6000,
+                        onClick: function() {
+                            window.focus()
+                            this.close()
+                        }
+                    })
+                }
+            }
 
             $('#message-list').append(time_str + content_str)
 
