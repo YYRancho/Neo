846e46525cbfb8da097c1a3011932967549f973d
Rancho <yyrancho@gmail.com>
2017-11-23 20:43:06 +0800

    fix(cinema): fix the error of video storage path recognition

--- cinema/views.py
+++ cinema/views.py
@@ -14,18 +14,21 @@ def get_cinema(request):
     return render(request, 'cinema/cinema.html')
 
 def get_repo(request):
-    repo_name_list = os.listdir('./cinema/static/storage')
+    # Determine it is in development mode or in deployment mode
+    path = './static/storage/'
+    if not os.path.exists(path):
+        path = './cinema/static/storage/'
+
     repo_list = []
+    repo_name_list = os.listdir(path)
     for repo_name in repo_name_list:
-        if not os.path.isdir('./cinema/static/storage/' + repo_name):
+        if not os.path.isdir(path + repo_name):
             continue
 
-        for file_name in os.listdir('./cinema/static/storage/' + repo_name):
+        for file_name in os.listdir(path + repo_name):
             if re.search(r'.*?json$', file_name):
-                with open('./cinema/static/storage/{}/{}'.format(repo_name, file_name), 'r') as f:
+                with open(path + '{}/{}'.format(repo_name, file_name), 'r') as f:
                     repo = json.loads(f.read())
                 repo_list.append(repo)
-
-    print(repo_list)
 
     return HttpResponse(json.dumps(repo_list))
