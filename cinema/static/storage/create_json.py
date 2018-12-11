ab472d080d490c357ea3bcf7a19b3f6ac94880b8
Rancho <yyrancho@gmail.com>
2018-12-11 23:48:20 +0800

    fix(cinema): fix the back-end crawler of movie metadata
    This bug is caused by the upgrade from Python 2 to Python 3. Due to the
    different definition of bytes and str, we deal with text and image
    obtained by the crawler in different ways.

--- cinema/static/storage/create_json.py
+++ cinema/static/storage/create_json.py
@@ -30,13 +30,13 @@ for repo_name in os.listdir('.'):
 
                 headers = {'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/47.0.2526.106 Chrome/47.0.2526.106 Safari/537.36'}
                 res = requests.get('https://www.douban.com/search?q=' + repo_name, headers=headers)
-                raw_douban_link = re.findall(r'<div class="title">[\s\S]*?<a href="(http.*?)"', res.content)[0]
+                raw_douban_link = re.findall(r'<div class="title">[\s\S]*?<a href="(http.*?)"', res.text)[0]
 
                 res = requests.get(raw_douban_link, headers=headers)
                 repo['douban_link'] = res.url # get the redirected link
                 repo['name'] = ' '.join(re.findall('h1[\s\S]*?<span.*?>(.*?)</span>[\s\S]*?<span class="year">(.*?)</span>', res.text)[0])
 
-                poster_url = re.findall(r'h1[\s\S]*?<img src="(http.*?)"', res.content)[0]
+                poster_url = re.findall(r'h1[\s\S]*?<img src="(http.*?)"', res.text)[0]
                 res = requests.get(poster_url, headers=headers)
                 format_postfix = re.findall(r'.*?(\.[a-zA-Z]+)$', poster_url)[0]
                 with open('./{}/{}'.format(repo_name, repo_name + format_postfix), 'wb') as f:
